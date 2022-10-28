b9a28e3083f45178983b614a3a9f51d2cd593ff0
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2022-10-27 20:50:25 -0400

    added basic cloning function for adding new items

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
 function addItem () {
+  // get item fields and clone
+  const newItem = document.querySelector('#itemList');
+  const clone = newItem.cloneNode(true);
+
+  // update new item id
+  clone.id = 'nextItem';
+
+  // add to DOM
+  newItem.after(clone);
 }
